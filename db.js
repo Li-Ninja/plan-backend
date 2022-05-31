9b0747629617a0015bacf959cfb82b8381652918
Ninja <lininja.tw@gmail.com>
2022-05-31 10:09:15 +0800

    feat: module export db.js

--- db.js
+++ db.js
@@ -1,13 +1,24 @@
 const mongodb = require('mongodb');
 const url = 'mongodb+srv://root:rootau4a83@mycluster.xr8zjdk.mongodb.net/?retryWrites=true&w=majority';
 const client = new mongodb.MongoClient(url);
+const objectId = mongodb.ObjectId;
+let dbConnection = null;
 
-client.connect(async function(err) {
-  if(err) {
-    console.log('link error', err);
-    return;
+module.exports = {
+  connectToServer: (callback) => {
+    client.connect((err, db) => {
+      if (err || !db) {
+        return callback(err);
+      }
+    })
+
+    dbConnection = db.db('Plan');
+    console.log('Successfully connected to MongoDB.');
+
+    return callback;
+  },
+
+  getDb: () => {
+    return dbConnection;
   }
-
-  console.log('link success');
-  client.close();
-});
+}
