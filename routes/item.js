369d74a92cafb6a7e47feca6f8276a2ded282ec0
Ninja <lininja.tw@gmail.com>
2022-06-10 22:59:53 +0800

    feat update item collection

--- routes/item.js
+++ routes/item.js
@@ -1,15 +1,40 @@
 const express = require('express');
+const dayjs = require('dayjs');
 const Item = require('../models/item.model');
-const db = require('../db');
 const router = express.Router();
 
 // get
 router.get('/Get', async (_req, res) => {
     const resData = await Item.find();
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(resData));
-    res.end();
+    res.send({
+        Success: true,
+        Data: resData
+    });
+});
+
+router.post('/Update', async (req, res) => {
+    const id = req.body._id;
+
+    if (!id) {
+        res.status(404).send('error');
+    }
+
+    const { count } = await Item.findById(id);
+
+    await Item.findByIdAndUpdate(id, {
+        count: count + 1,
+        updateAt: dayjs()
+    }).then(() => {
+        res.send({
+            Success: true
+        })
+    }).catch(() => {
+        res.send({
+            Success: false,
+            Message: 'update failed'
+        })
+    });
 });
 
 module.exports = router;
